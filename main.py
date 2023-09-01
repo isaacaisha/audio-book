0718878786313144c7df37dc1c0d10c3150b6ff9
isaacaisha <medusadbt@gmail.com>
2023-09-01 03:48:23 +0200

    add port to heroku

--- main.py
+++ main.py
@@ -141,4 +141,4 @@ def miserable_chapter2():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
